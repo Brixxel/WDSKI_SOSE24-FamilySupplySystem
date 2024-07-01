1f0ace25b5d61cae2ddcc685cbd495b268705605
Tom Weber <73034898+Rebtm@users.noreply.github.com>
2024-07-01 10:43:02 +0200

    translator added for api

---
+++ Techology_TEsting_Space/API/translate.py
@@ -0,0 +1,15 @@
+from deep_translator import DeeplTranslator
+import json
+
+key = ""
+with open("/Users/tom/Documents/GitHub/WDSKI_SOSE24-FamilySupplySystem/credentials.json", "r") as file:
+    data = json.load(file)
+    key = data["api_key_deepL"]
+
+
+
+texts = ["apfel", "bananen"]
+
+translated = DeeplTranslator(api_key=key, source="de", target="en").translate_batch(texts)
+
+print (translated)
